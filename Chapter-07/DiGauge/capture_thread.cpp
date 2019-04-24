09bd2e4bfa013148dceb574dad3167917ae88433
KDr2 <zhuo@hexoasis.com>
2019-04-24 10:09:59 +0800

    Ch07: DiGauge, measure distance in the view of BirdEye

--- Chapter-07/DiGauge/capture_thread.cpp
+++ Chapter-07/DiGauge/capture_thread.cpp
@@ -1,3 +1,6 @@
+#include <utility>
+#include <algorithm>
+
 #include <QTime>
 #include <QDebug>
 
@@ -74,6 +77,7 @@ static void decodeOutLayers(
     cv::Mat &frame, const vector<cv::Mat> &outs,
     vector<cv::Rect> &outBoxes
 );
+void distanceBirdEye(cv::Mat &frame, vector<cv::Rect> &cars);
 
 void CaptureThread::detectObjectsDNN(cv::Mat &frame)
 {
@@ -118,6 +122,7 @@ void CaptureThread::detectObjectsDNN(cv::Mat &frame)
     for(size_t i = 0; i < outBoxes.size(); i ++) {
         cv::rectangle(frame, outBoxes[i], cv::Scalar(0, 0, 255));
     }
+    distanceBirdEye(frame, outBoxes);
 }
 
 void decodeOutLayers(
@@ -163,5 +168,53 @@ void decodeOutLayers(
     for (size_t i = 0; i < indices.size(); ++i) {
         int idx = indices[i];
         outBoxes.push_back(boxes[idx]);
+    }
+}
+
+void distanceBirdEye(cv::Mat &frame, vector<cv::Rect> &cars)
+{
+    vector<int> length_of_cars;
+    vector<pair<int, int>> endpoints;
+    vector<pair<int, int>> cars_merged;
+
+    for (auto car: cars) {
+        length_of_cars.push_back(car.width);
+        endpoints.push_back(make_pair(car.x, 1));
+        endpoints.push_back(make_pair(car.x + car.width, -1));
+    }
+
+    sort(length_of_cars.begin(), length_of_cars.end());
+    int length = length_of_cars[cars.size() / 2];
+    sort(
+        endpoints.begin(), endpoints.end(),
+        [](pair<int, int> a, pair<int, int> b) {
+            return a.first < b.first;
+        }
+    );
+
+    int flag = 0, start = 0;
+    for (auto ep: endpoints) {
+        flag += ep.second;
+        if (flag == 1 && start == 0) { // a start
+            start = ep.first;
+        } else if (flag == 0) { // an end
+            cars_merged.push_back(make_pair(start, ep.first));
+            start = 0;
+        }
+    }
+
+    for (size_t i = 1; i < cars_merged.size(); i++) {
+        int x1 = cars_merged[i - 1].second; // head of car, start of spacing
+        int x2 = cars_merged[i].first; // end of another car, end of spacing
+        cv::line(frame, cv::Point(x1, 0), cv::Point(x1, frame.rows), cv::Scalar(0, 255, 0), 2);
+        cv::line(frame, cv::Point(x2, 0), cv::Point(x2, frame.rows), cv::Scalar(0, 0, 255), 2);
+        float distance = (x2 - x1) * (5.0 / length);
+
+        // display the label at the top of the bounding box
+        string label = cv::format("%.2f m", distance);
+        int baseLine;
+        cv::Size labelSize = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+        int label_x = (x1 + x2) / 2 - (labelSize.width / 2);
+        cv::putText(frame, label, cv::Point(label_x, 20), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
     }
 }
